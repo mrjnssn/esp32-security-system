6b0efa27cd0187e786ab091e985996e865ca282f
mrjnssn <210423376+mrjnssn@users.noreply.github.com>
2026-09-13 10:48:00 +0200

    connection states added to controller.ino

--- ESP_NOW_controller/ESP_NOW_controller.ino
+++ ESP_NOW_controller/ESP_NOW_controller.ino
@@ -4,12 +4,15 @@
 
 #define WIFI_CHANNEL 6
 
-enum connectionState {
+enum ConnectionState {
   DISCONNECTED,
   SYN_RECEIVED,
   CONNECTED,
   FIN_RECEIVED,
 };
+
+ConnectionState connectionState = DISCONNECTED;
+uint16_t currentSequence = 42;
 
 void onDataReceived(const esp_now_recv_info_t *info, const uint8_t *data, int len) {
   PacketHeader receivedHeader; 
@@ -28,24 +31,39 @@ void onDataReceived(const esp_now_recv_info_t *info, const uint8_t *data, int le
     return;
   }
 
-  if (receivedHeader.type != MSG_SYN) {
-    return;
+  switch (receivedHeader.type) {
+    case MSG_SYN:
+      if (connectionState != DISCONNECTED) {
+        return;
+      }
+      
+      handleSyn(info, receivedHeader);
+
+      break;
+    case MSG_ACK:
+      if (connectionState != SYN_RECEIVED) {
+        return;
+      }
+
+      handleAck(info, receivedHeader);
+
+      break;
+    case MSG_FIN:
+      if (connectionState != CONNECTED) {
+        return;
+      }   
+
+      handleFin(info, receivedHeader);
+
+      break;
   }
+}
+
+void handleSyn(const esp_now_recv_info_t *info, const PacketHeader& receivedHeader) {
+  // confirm reception of SYN
+  printConfirmation(receivedHeader);
   
-  Serial.println("Valid SEC32 SYN received");
-  Serial.print("version: ");
-  Serial.println(receivedHeader.version);
-  Serial.print("source: ");
-  Serial.println(receivedHeader.source);
-  Serial.print("destination: ");
-  Serial.println(receivedHeader.destination);
-  Serial.print("sequence: ");
-  Serial.println(receivedHeader.sequence);
-  Serial.print("payloadLength: ");
-  Serial.println(receivedHeader.payloadLength);
-
   // add source as peer
-
   if (!esp_now_is_peer_exist(info->src_addr)) {
 
     esp_now_peer_info_t peerInfo = {}; // initialize peerInfo struct
@@ -60,6 +78,10 @@ void onDataReceived(const esp_now_recv_info_t *info, const uint8_t *data, int le
       }
   }
 
+  // update sequence
+  currentSequence = receivedHeader.sequence;
+
+  // create response: SYN_ACK packet header
   PacketHeader synAckHeader;
 
   synAckHeader.version = PROTOCOL_VERSION;
@@ -69,16 +91,86 @@ void onDataReceived(const esp_now_recv_info_t *info, const uint8_t *data, int le
   synAckHeader.sequence = receivedHeader.sequence;
   synAckHeader.payloadLength = 0;
 
+  // create space in memory for response
   uint8_t responseBuffer[HEADER_SIZE];
 
+  // serialise and store response in buffer
   serializeHeader(synAckHeader, responseBuffer);
 
+  // send response
   esp_now_send(
     info->src_addr,
     responseBuffer,
     HEADER_SIZE
   );
 
+  // update state machine
+  connectionState = SYN_RECEIVED;
+}
+
+void handleAck(const esp_now_recv_info_t *info, const PacketHeader& receivedHeader) {
+  // confirm reception of SYN
+  printConfirmation(receivedHeader);
+
+  // check sequence
+  if (receivedHeader.sequence != currentSequence) {
+    return;
+  }
+
+  // update state machine
+  connectionState = CONNECTED;
+}
+
+void handleFin(const esp_now_recv_info_t *info, const PacketHeader& receivedHeader) {
+  // confirm reception of SYN
+  printConfirmation(receivedHeader);
+
+  // update sequence
+  currentSequence = receivedHeader.sequence;
+
+  // create response: FIN_ACK packet header
+  PacketHeader finAckHeader;
+
+  finAckHeader.version = PROTOCOL_VERSION;
+  finAckHeader.source = NODE_CONTROLLER;
+  finAckHeader.destination = receivedHeader.source;
+  finAckHeader.type = MSG_FIN_ACK;
+  finAckHeader.sequence = receivedHeader.sequence;
+  finAckHeader.payloadLength = 0;
+
+  // create space in memory for response
+  uint8_t responseBuffer[HEADER_SIZE];
+
+  // serialise and store response in buffer
+  serializeHeader(finAckHeader, responseBuffer);
+
+  // send response
+  esp_now_send(
+    info->src_addr,
+    responseBuffer,
+    HEADER_SIZE
+  );
+
+  // update state machine
+  connectionState = DISCONNECTED;
+
+  println("")
+}
+
+void printConfirmation(const PacketHeader& receivedHeader) {
+  Serial.print("Valid SEC32 ");
+  Serial.print(receivedHeader.type);
+  Serial.println(" received");
+  Serial.print("version: ");
+  Serial.println(receivedHeader.version);
+  Serial.print("source: ");
+  Serial.println(receivedHeader.source);
+  Serial.print("destination: ");
+  Serial.println(receivedHeader.destination);
+  Serial.print("sequence: ");
+  Serial.println(receivedHeader.sequence);
+  Serial.print("payloadLength: ");
+  Serial.println(receivedHeader.payloadLength);
 }
 
 void setup() {
